588a0dc13bb838b73cb89e6dadcc4f2077b4be80
PI and more <piandmore@j-tools.net>
2016-10-25 09:59:24 +0200

    Update documentation

--- PAM-Arduino-WS2812-Small/PAM-Arduino-WS2812-Small.ino
+++ PAM-Arduino-WS2812-Small/PAM-Arduino-WS2812-Small.ino
@@ -1,15 +1,33 @@
+//
+// The library from Adafruit which enables the use of the WS2812
+// or NeoPixel
+//
 #include <Adafruit_NeoPixel.h>
 
+//
+// The pin the circle of WS2812 leds is connected to
+//
 #define CIRCLEPIN 6
-
+//
+// The number of leds in our circle
+//
 #define PIXELCOUNT 16
 
+//
+// Initialize the Adafruit library
+//
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(PIXELCOUNT,CIRCLEPIN,
   NEO_GRB+NEO_KHZ800);
 
+//
+// The current pixel
+//
 byte thisPixel = 0;
 
 void setup() {
+  //
+  // Start the led string and turn them all off
+  //
   pixels.begin();
   for(int i=0;i<PIXELCOUNT;i++){
     pixels.setPixelColor(i, pixels.Color(0,0,0));
@@ -18,11 +36,19 @@ void setup() {
 }
 
 void loop() {
+  //
+  // Set the pixels with a trailing tail
+  // Since we paint the last pixel black, we do not need to initialize
+  // all pixels to black
+  //
   pixels.setPixelColor((thisPixel+4)%16,pixels.Color(255,0,0));
   pixels.setPixelColor((thisPixel+3)%16,pixels.Color(100,0,0));
   pixels.setPixelColor((thisPixel+2)%16,pixels.Color(40,0,0));
   pixels.setPixelColor((thisPixel+1)%16,pixels.Color(15,0,0));
   pixels.setPixelColor((thisPixel)%16,pixels.Color(0,0,0));
+  //
+  // Don't forget to call the pixels.show();
+  //
   pixels.show();
   delay(30);
   thisPixel++;
--- PAM-Arduino-WS2812/PAM-Arduino-WS2812.ino
+++ PAM-Arduino-WS2812/PAM-Arduino-WS2812.ino
@@ -1,16 +1,34 @@
+//
+// The library from Adafruit which enables the use of the WS2812
+// or NeoPixel
+//
 #include <Adafruit_NeoPixel.h>
 
+//
+// The pin the circle of WS2812 leds is connected to
+//
 #define CIRCLEPIN 6
-
+//
+// The number of leds in our circle
+//
 #define PIXELCOUNT 16
 
+//
+// Initialize the Adafruit library
+//
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(PIXELCOUNT,CIRCLEPIN,
   NEO_GRB+NEO_KHZ800);
 
+//
+// The current position in degrees
+//
 int degree = 0;
 
 void setup() {
   Serial.begin(115200);
+  //
+  // Start the led string and turn them all off
+  //
   pixels.begin();
   for(int i=0;i<PIXELCOUNT;i++){
     pixels.setPixelColor(i, pixels.Color(0,0,0));
@@ -20,14 +38,26 @@ void setup() {
 
 void loop() {
   Serial.println(degree);
+  //
+  // Turn all leds to black
+  //
   for(int i=0;i<PIXELCOUNT;i++){
     pixels.setPixelColor(i, pixels.Color(0,0,0));
   }
-  int thisPixel = (degree*16)/360;
+  //
+  // Convert degrees to the pixel position
+  //
+  int thisPixel = (degree*PIXELCOUNT)/360;
+  //
+  // Set the pixels with a trailing tail
+  //
   pixels.setPixelColor((thisPixel+3)%16,pixels.Color(255,0,0));
   pixels.setPixelColor((thisPixel+2)%16,pixels.Color(100,0,0));
   pixels.setPixelColor((thisPixel+1)%16,pixels.Color(40,0,0));
   pixels.setPixelColor((thisPixel)%16,pixels.Color(15,0,0));
+  //
+  // Don't forget to call the pixels.show();
+  //
   pixels.show();
   delay(50);
   degree += 10;
